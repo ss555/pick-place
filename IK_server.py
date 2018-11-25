602409f4d4a38902c90a207e021d493cbda1063e
ISRAILOV SARDOR <5sardorisrailov5@gmail.com>
2018-11-25 23:43:33 +0100

    Add files via upload

--- IK_server.py
+++ IK_server.py
@@ -23,8 +23,58 @@ def TF_Matrix(alpha,a,d,q):
 				[sin(q)*sin(alpha),	cos(q)*sin(alpha),	cos(alpha),		cos(alpha)*d],
 				[0,					0,					0,				1]])
 	return TF
+d1,d2,d3,d4,d5,d6,d7=symbols('d1:8')#link offset
+a0,a1,a2,a3,a4,a5,a6=symbols('a0:7')#link length
+alpha0,alpha1,alpha2,alpha3,alpha4,alpha5,alpha6=symbols('alpha0:7')#twist(orientation angle)
+q1,q2,q3,q4,q5,q6,q7=symbols('q1:8')#symbols for actual revolute angles
+DH_Table= {  alpha0:0,			a0:0,		d1:0.75,	q1:q1,
+alpha1:-pi/2.,		a1:0.35,	d2:0,		q2:-pi/2.+q2,
+			alpha2:0,			a2:1.25,	d3:0,		q3:q3,
+			alpha3:-pi/2.,		a3:-0.054,	d4:1.5,		q4:q4,
+			alpha4:pi/2.,		a4:0,		d5:0,		q5:q5,
+			alpha5:-pi/2.,		a5:0,		d6:0,		q6:q6,
+			alpha6:0,			a6:0,		d7:0.303,	q7:0}
+#transformation matrix of Ti-(i-1)...FK
+T0_1=TF_Matrix(alpha0,a0,d1,q1).subs(DH_Table)
+T1_2=TF_Matrix(alpha1,a1,d2,q2).subs(DH_Table)
+T2_3=TF_Matrix(alpha2,a2,d3,q3).subs(DH_Table)
+#T3_4=TF_Matrix(alpha3,a3,d4,q4).subs(DH_Table)
+#T4_5=TF_Matrix(alpha4,a4,d5,q5).subs(DH_Table)
+#T5_6=TF_Matrix(alpha5,a5,d6,q6).subs(DH_Table)
+#T6_EE=TF_Matrix(alpha6,a6,d7,q7).subs(DH_Table)
+#T0_EE=T0_1*T1_2*T2_3*T3_4*T4_5*T5_6*T6_EE#transformation matrix from base to end effector is the multiplication of separate TF between coordinate systems
+r, p, y = symbols('r p y')
 
+ROT_x = Matrix([[1, 0, 0],
+				[0, cos(r), -sin(r)],
+				[0, sin(r), cos(r)]])#roll
+ROT_y = Matrix([[cos(p), 0, sin(p)],
+				[0,		1,	0],
+				[-sin(p),	0,	cos(p)]])#Pitch
+ROT_z = Matrix([[cos(y),	-sin(y),	0],
+				[sin(y),	cos(y),		0],
+				[0,			0,			1]])#YAW
+ROT_EE=ROT_z*ROT_y*ROT_x
+#rotation error
+Rot_Corr = ROT_z.subs(y, radians(180))*ROT_y.subs(p, radians(-90))
+
+
+ROT_EE=ROT_EE*Rot_Corr
+
+side_c=1.25
+side_a=1.501
+sag_angle=atan2(0.054, 1.50)
+#
+###
+R0_3=T0_1[0:3,0:3]*T1_2[0:3,0:3]*T2_3[0:3,0:3]
 def handle_calculate_IK(req):
+    global ROT_EE
+    global side_c
+    global side_a
+    global sag_angle
+    global Rot_Corr
+    global R0_3
+
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
     if len(req.poses) < 1:
         print "No valid poses received"
@@ -34,54 +84,11 @@ def handle_calculate_IK(req):
         ### Your FK code here
         # Create symbols
         #define DH
-	d1,d2,d3,d4,d5,d6,d7=symbols('d1:8')#link offset
-	a0,a1,a2,a3,a4,a5,a6=symbols('a0:7')#link length
-	alpha0,alpha1,alpha2,alpha3,alpha4,alpha5,alpha6=symbols('alpha0:7')#twist(orientation angle)
-	q1,q2,q3,q4,q5,q6,q7=symbols('q1:8')#symbols for actual revolute angles
-	DH_Table= {  alpha0:0,			a0:0,		d1:0.75,	q1:q1,
-	alpha1:-pi/2.,		a1:0.35,	d2:0,		q2:-pi/2.+q2,
-				alpha2:0,			a2:1.25,	d3:0,		q3:q3,
-				alpha3:-pi/2.,		a3:-0.054,	d4:1.5,		q4:q4,
-				alpha4:pi/2.,		a4:0,		d5:0,		q5:q5,
-				alpha5:-pi/2.,		a5:0,		d6:0,		q6:q6,
-				alpha6:0,			a6:0,		d7:0.303,	q7:0}
-	#transformation matrix of Ti-(i-1)...FK
-	T0_1=TF_Matrix(alpha0,a0,d1,q1).subs(DH_Table)
-	T1_2=TF_Matrix(alpha1,a1,d2,q2).subs(DH_Table)
-	T2_3=TF_Matrix(alpha2,a2,d3,q3).subs(DH_Table)
-	T3_4=TF_Matrix(alpha3,a3,d4,q4).subs(DH_Table)
-	T4_5=TF_Matrix(alpha4,a4,d5,q5).subs(DH_Table)
-	T5_6=TF_Matrix(alpha5,a5,d6,q6).subs(DH_Table)
-	T6_EE=TF_Matrix(alpha6,a6,d7,q7).subs(DH_Table)
-	T0_EE=T0_1*T1_2*T2_3*T3_4*T4_5*T5_6*T6_EE#transformation matrix from base to end effector is the multiplication of separate TF between coordinate systems
-	r, p, y = symbols('r p y')
 
-	ROT_x = Matrix([[1, 0, 0],
-					[0, cos(r), -sin(r)],
-					[0, sin(r), cos(r)]])#roll
-	ROT_y = Matrix([[cos(p), 0, sin(p)],
-					[0,		1,	0],
-					[-sin(p),	0,	cos(p)]])#Pitch
-	ROT_z = Matrix([[cos(y),	-sin(y),	0],
-					[sin(y),	cos(y),		0],
-					[0,			0,			1]])#YAW
-	ROT_EE=ROT_z*ROT_y*ROT_x
-	#rotation error
-	Rot_Corr = ROT_z.subs(y, radians(180))*ROT_y.subs(p, radians(-90))
-
-
-	ROT_EE=ROT_EE*Rot_Corr
-
-	side_c=1.25
-	side_a=1.501
-	sag_angle=atan2(0.054, 1.50)
-	#
-        ###
-	R0_3=T0_1[0:3,0:3]*T1_2[0:3,0:3]*T2_3[0:3,0:3]
         # Initialize service response
         joint_trajectory_list = []
         for x in xrange(0, len(req.poses)):
-# IK code starts here
+		# IK code starts here
 		joint_trajectory_point = JointTrajectoryPoint()
 
 		
